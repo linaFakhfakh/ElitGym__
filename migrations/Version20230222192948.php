06a5b9911a447854b2be99306ea005a7aaaafb06
linaFakhfakh <lina.fakhfakh@esprit.tn>
2023-02-23 09:40:35 +0100

    Ajouter les abonnements

---
+++ migrations/Version20230222192948.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230222192948 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE abonnement (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(20) NOT NULL, type VARCHAR(20) NOT NULL, prenom VARCHAR(255) NOT NULL, numero INT NOT NULL, email VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE offre (id INT AUTO_INCREMENT NOT NULL, abonnement_nom_id INT DEFAULT NULL, points INT NOT NULL, prix DOUBLE PRECISION NOT NULL, pourcentage DOUBLE PRECISION NOT NULL, date_debut DATE NOT NULL, date_fin DATE NOT NULL, INDEX IDX_AF86866F2A21A176 (abonnement_nom_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE offre ADD CONSTRAINT FK_AF86866F2A21A176 FOREIGN KEY (abonnement_nom_id) REFERENCES abonnement (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE offre DROP FOREIGN KEY FK_AF86866F2A21A176');
+        $this->addSql('DROP TABLE abonnement');
+        $this->addSql('DROP TABLE offre');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
---
+++ src/Controller/AbonnementController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+use App\Entity\Abonnement;
+use App\Controller\HomeController;
+use App\Repository\AbonnementRepository;
+use App\Form\AbonnementFormType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Command\AboutCommand;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+#[Route('/abonnement')]
+class AbonnementController extends AbstractController
+{
+    #[Route('/', name: 'app_abonnement_index', methods: ['GET'])]
+    public function index(AbonnementRepository $abonnementRepository): Response
+    {
+        return $this->render('abonnement/index.html.twig', [
+            'abonnements' => $abonnementRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_abonnement_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, AbonnementRepository $abonnementRepository): Response
+    {
+        $abonnement = new Abonnement();
+        $form = $this->createForm(AbonnementFormType::class, $abonnement);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $abonnementRepository->save($abonnement, true);
+
+            return $this->redirectToRoute('app_abonnement_new', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('abonnement/new.html.twig', [
+            'abonnement' => $abonnement,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_abonnement_show', methods: ['GET'])]
+    public function show(Abonnement $abonnement): Response
+    {
+        return $this->render('abonnement/show.html.twig', [
+            'abonnement' => $abonnement,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_abonnement_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Abonnement $abonnement, AbonnementRepository $abonnementRepository): Response
+    {
+        $form = $this->createForm(AbonnementFormType::class, $abonnement);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $abonnementRepository->save($abonnement, true);
+
+            return $this->redirectToRoute('app_abonnement_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('abonnement/edit.html.twig', [
+            'abonnement' => $abonnement,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_abonnement_delete', methods: ['POST'])]
+    public function delete(Request $request, Abonnement $abonnement, AbonnementRepository $abonnementRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$abonnement->getId(), $request->request->get('_token'))) {
+            $abonnementRepository->remove($abonnement, true);
+        }
+
+        return $this->redirectToRoute('app_abonnement_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
